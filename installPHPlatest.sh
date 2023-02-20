4c7f6bcaab6cc8c28d23dbbe37f4f39d34cad746
m-agoumi <magoumi@MACAS1L23069.groupejet.priv>
2023-02-20 08:49:59 +0100

    add a script to install latest version of php

---
+++ installPHPlatest.sh
@@ -0,0 +1,17 @@
+## updating repos
+if [ "$EUID" -ne 0 ]
+	echo "you will be prompt to enter sudo password"
+	sudo apt update
+	sudo apt install -y software-properties-common
+	sudo add-apt-repository ppa:ondrej/php
+	sudo apt update
+	echo "installing latest PHP version"
+	sudo apt install php
+else
+	apt update
+        apt install -y software-properties-common
+        add-apt-repository ppa:ondrej/php
+        apt update
+        echo "installing latest PHP version"
+        apt install php
+fi
